068beb5ca02206a113d1e5bb8cc0a3dbbbf25c04
tiobagio <tio.bagio@gmail.com>
2020-07-28 19:49:01 +0800

    update to suit emea-demo 5.0.5

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,7 @@
 #
 powershell_script 'Set host file so the instance knows where to find chef-server' do
     code <<-EOH
-    $hosts = "172.31.54.10 chef.automate-demo.com"
+    $hosts = "172.31.54.57 chef.automate-demo.com"
     $file = "C:\\Windows\\System32\\drivers\\etc\\hosts"
     $hosts | Add-Content $file
     EOH
@@ -28,9 +28,9 @@ powershell_script 'Set host file so the instance knows where to find chef-server
     $nodeName = "Win-YOURNAME-{0}" -f (-join ((65..90) + (97..122) | Get-Random -Count 4 | % {[char]$_}))
   
     $clientrb = @"
-chef_server_url 'https://chef.automate-demo.com/organizations/automate'
-validation_client_name 'automate-validator'
-validation_key 'C:\\Users\\Administrator\\AppData\\Local\\Temp\\kitchen\\cookbooks\\bjc_windows_bootstrap\\recipes\\validator.pem'
+chef_server_url 'https://chef.automate-demo.com/organizations/automate-org'
+validation_client_name 'automate-org-validator'
+validation_key 'C:\\Users\\Administrator\\AppData\\Local\\Temp\\kitchen\\cookbooks\\bjc_windows_bootstrap\\recipes\\automate_org_validator.pem'
 node_name '{0}'
 ssl_verify_mode :verify_none
 chef_license 'accept'
